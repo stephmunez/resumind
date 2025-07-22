98f87fd40e2cdb98eeb573957464f16cbc397edf
Stephen Muñez <sjtmunez@gmail.com>
2025-07-22 13:37:52 +0800

    feat: auth and puter api

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,26 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import Navbar from '~/components/Navbar';
 import ResumeCard from '~/components/ResumeCard';
+import { usePuterStore } from '~/lib/puter';
 import { resumes } from '../../constants';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: 'Resumind' },
-    {
-      name: 'description',
-      content: 'Smart resume feedback for your dream job!',
-    },
+    { name: 'description', content: 'Smart feedback for your dream job!' },
   ];
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
